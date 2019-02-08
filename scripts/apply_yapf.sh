23a935ebfdd58d791bee1f3903624d34b5ff4938
Gregor Simm <gncsimm@gmail.com>
2019-02-08 14:59:11 +0000

    update name of directory in yapf script

--- scripts/apply_yapf.sh
+++ scripts/apply_yapf.sh
@@ -3,4 +3,4 @@
 # Apply YAPF recursively and inplace to all relevant files
 # To be executed from the root directory
 
-yapf --in-place --recursive --style=".style.yapf" setup.py aladdin_bo
+yapf --in-place --recursive --style=".style.yapf" setup.py boa
