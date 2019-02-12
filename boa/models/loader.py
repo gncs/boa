290cd6a0c550b10af86e2a745fc1b3a4ee2f84e6
Gregor Simm <gncsimm@gmail.com>
2019-02-12 16:31:25 +0000

    add type annotation to load_model function

--- boa/models/loader.py
+++ boa/models/loader.py
@@ -12,7 +12,7 @@ class ModelLoaderError(Exception):
         super().__init__("Cannot load model: " + message)
 
 
-def load_model(config):
+def load_model(config: dict):
     try:
         return _models[config['name']](**config.get('parameters', {}))
     except KeyError as e:
