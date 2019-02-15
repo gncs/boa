c88bd3d3ad94acdfe3cbbff4a382116b85709ff9
Gregor Simm <gncsimm@gmail.com>
2019-02-15 11:52:29 +0000

    add output parser and tests

--- boa/objective/gem5_aladdin/process.py
+++ boa/objective/gem5_aladdin/process.py
@@ -1,57 +1,56 @@
 import re
 
-output_regexps = [
-    re.compile(r'Cycle : (.*)\scycles.*'),
-    re.compile(r'Avg Power: (.*)\smW.*'),
-    re.compile(r'Idle FU Cycles: (.*)\scycles.*'),
-    re.compile(r'Avg FU Power: (.*)\smW.*'),
-    re.compile(r'Avg FU Dynamic Power: (.*)\smW.*'),
-    re.compile(r'Avg FU leakage Power: (.*)\smW.*'),
-    re.compile(r'Avg MEM Power: (.*)\smW.*'),
-    re.compile(r'Avg MEM Dynamic Power: (.*)\smW.*'),
-    re.compile(r'Avg MEM Leakage Power: (.*)\smW.*'),
-    re.compile(r'Total Area: (.*)\suM\^2.*'),
-    re.compile(r'FU Area: (.*)\suM\^2.*'),
-    re.compile(r'MEM Area: (.*)\suM\^2.*'),
-    re.compile(r'Num of Double Precision FP Multipliers: (.*)\s.*'),
-    re.compile(r'Num of Double Precision FP Adders: (.*)\s.*'),
-    re.compile(r'Num of Trigonometric Units: (.*)\s.*'),
-    re.compile(r'Num of Bit-wise Operators \(32-bit\): (.*)\s.*'),
-    re.compile(r'Num of Shifters \(32-bit\): (.*)\s.*'),
-    re.compile(r'Num of Registers \(32-bit\): (.*)\s.*')
-]
+output_regexps = {
+    'cycles': re.compile(r'Cycle : (.*) cycles'),
+    'avg_power': re.compile(r'Avg Power: (.*) mW'),
+    'idle_fu_cycles': re.compile(r'Idle FU Cycles: (.*) cycles'),
+    'avg_fu_cycles': re.compile(r'Avg FU Power: (.*) mW'),
+    'avg_fu_dynamic_power': re.compile(r'Avg FU Dynamic Power: (.*) mW'),
+    'avg_fu_leakage_power': re.compile(r'Avg FU leakage Power: (.*) mW'),
+    'avg_mem_power': re.compile(r'Avg MEM Power: (.*) mW'),
+    'avg_mem_dynamic_power': re.compile(r'Avg MEM Dynamic Power: (.*) mW'),
+    'avg_mem_leakage_power': re.compile(r'Avg MEM Leakage Power: (.*) mW'),
+    'total_area': re.compile(r'Total Area: (.*) uM\^2'),
+    'fu_area': re.compile(r'FU Area: (.*) uM\^2'),
+    'mem_area': re.compile(r'MEM Area: (.*) uM\^2'),
+    'num_double_precision_fp_multipliers': re.compile(r'Num of Double Precision FP Multipliers: (.*)\s'),
+    'num_double_precision_fp_adders': re.compile(r'Num of Double Precision FP Adders: (.*)'),
+    'num_trigonometric_units': re.compile(r'Num of Trigonometric Units: (.*)'),
+    'num_bit-wise_operators_32': re.compile(r'Num of Bit-wise Operators \(32-bit\): (.*)'),
+    'num_shifters_32': re.compile(r'Num of Shifters \(32-bit\): (.*)'),
+    'num_registers_32': re.compile(r'Num of Registers \(32-bit\): (.*)'),
+}
 
-header_regexps = [
-    re.compile(r'set cycle_time (.*)\s.*'),
-    re.compile(r'set pipelining (.*)\s.*'),
-    re.compile(r'set cache_size (.*)\s.*'),
-    re.compile(r'set cache_assoc (.*)\s.*'),
-    re.compile(r'set cache_hit_latency (.*)\s.*'),
-    re.compile(r'set cache_line_sz (.*)\s.*'),
-    re.compile(r'set cache_queue_size (.*)\s.*'),
-    re.compile(r'set tlb_hit_latency (.*)\s.*'),
-    re.compile(r'set tlb_miss_latency (.*)\s.*'),
-    re.compile(r'set tlb_page_size (.*)\s.*'),
-    re.compile(r'set tlb_entries (.*)\s.*'),
-    re.compile(r'set tlb_max_outstanding_walks (.*)\s.*'),
-    re.compile(r'set tlb_assoc (.*)\s.*'),
-    re.compile(r'set tlb_bandwidth (.*)\s.*'),
-    re.compile(r'set l2cache_size (.*)\s.*'),
-    re.compile(r'set enable_l2 (.*)\s.*'),
-    re.compile(r'set pipelined_dma (.*)\s.*'),
-    re.compile(r'set ignore_cache_flush (.*)\s.*')
-]
+header_regexps = {
+    'cycle_time': re.compile(r'set cycle_time (.*)\s.*'),
+    'pipelining': re.compile(r'set pipelining (.*)\s.*'),
+    'cache_size': re.compile(r'set cache_size (.*)\s.*'),
+    'cache_assoc': re.compile(r'set cache_assoc (.*)\s.*'),
+    'cache_hit_latency': re.compile(r'set cache_hit_latency (.*)\s.*'),
+    'cache_line_sz': re.compile(r'set cache_line_sz (.*)\s.*'),
+    'cache_queue_size': re.compile(r'set cache_queue_size (.*)\s.*'),
+    'tlb_hit_latency': re.compile(r'set tlb_hit_latency (.*)\s.*'),
+    'tlb_miss_latency': re.compile(r'set tlb_miss_latency (.*)\s.*'),
+    'tlb_page_size': re.compile(r'set tlb_page_size (.*)\s.*'),
+    'tlb_entries': re.compile(r'set tlb_entries (.*)\s.*'),
+    'tlb_max_outstanding_walks': re.compile(r'set tlb_max_outstanding_walks (.*)\s.*'),
+    'tlb_assoc': re.compile(r'set tlb_assoc (.*)\s.*'),
+    'tlb_bandwidth': re.compile(r'set tlb_bandwidth (.*)\s.*'),
+    'l2cache_size': re.compile(r'set l2cache_size (.*)\s.*'),
+    'enable_l2': re.compile(r'set enable_l2 (.*)\s.*'),
+    'pipelined_dma': re.compile(r'set pipelined_dma (.*)\s.*'),
+    'ignore_cache_flush': re.compile(r'set ignore_cache_flush (.*)\s.*'),
+}
 
 
-def parse_file(filename, regexps):
-    with open(filename) as f:
+def parse_file(file_path: str, regexps: dict) -> dict:
+    with open(file_path) as f:
         contents = f.read()
 
-    results = []
-    for regexp in regexps:
+    results = {}
+    for key, regexp in regexps.items():
         try:
-            m = regexp.search(contents)
-            results.append(float(m.groups(0)[0]))
+            results[key] = float(regexp.search(contents).groups()[0])
         except Exception:
             raise Exception("Failed to parse regular expression '" + regexp.pattern + "'")
 
---
+++ boa/objective/gem5_aladdin/tests/test_parser.py
@@ -0,0 +1,44 @@
+import os
+from unittest import TestCase
+
+import pkg_resources
+
+from boa.objective.gem5_aladdin.process import parse_file, output_regexps
+
+
+class TestParser(TestCase):
+    RESOURCES_DIR = pkg_resources.resource_filename(__package__, 'resources')
+
+    def test_file_does_not_exist(self):
+        path = os.path.join(self.RESOURCES_DIR, 'not_a_file.yaml')
+        with self.assertRaises(Exception):
+            parse_file(file_path=path, regexps=output_regexps)
+
+    def test_parse_output(self):
+        path = os.path.join(self.RESOURCES_DIR, 'gem5_aladdin.output')
+
+        expected = {
+            'cycles': 51439,
+            'avg_power': 39.251,
+            'idle_fu_cycles': 35951,
+            'avg_fu_cycles': 34.1792,
+            'avg_fu_dynamic_power': 23.605,
+            'avg_fu_leakage_power': 10.5742,
+            'avg_mem_power': 5.07184,
+            'avg_mem_dynamic_power': 3.4128,
+            'avg_mem_leakage_power': 1.65904,
+            'total_area': 1.37064E06,
+            'fu_area': 1.04667E06,
+            'mem_area': 323976,
+            'num_double_precision_fp_multipliers': 37,
+            'num_double_precision_fp_adders': 30,
+            'num_trigonometric_units': 14,
+            'num_bit-wise_operators_32': 9,
+            'num_shifters_32': 6,
+            'num_registers_32': 192,
+        }
+
+        results = parse_file(file_path=path, regexps=output_regexps)
+
+        for k, v in results.items():
+            self.assertAlmostEqual(v, expected[k])
