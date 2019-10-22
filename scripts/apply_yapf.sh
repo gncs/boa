6636be67eb5c91ad03e4b9600200b06545ee5756
Gregor Simm <gncsimm@gmail.com>
2019-10-22 09:59:49 +0100

    remove scripts folder

--- scripts/apply_yapf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Apply YAPF recursively and inplace to all relevant files
-# To be executed from the root directory
-
-yapf --in-place --recursive --style=".style.yapf" setup.py boa
