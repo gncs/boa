c27304f92d510f8c117441fca1564f3f3a27da1d
Gregor Simm <gncsimm@gmail.com>
2019-10-23 19:06:12 +0100

    remove pygmo dependence

--- boa/acquisition/tests/test_hypervolume.py
+++ boa/acquisition/tests/test_hypervolume.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 
 import numpy as np
-import pygmo
 
 from boa.acquisition.util import calculate_hypervolume
 
@@ -17,11 +16,22 @@ class TestHyperVolume(TestCase):
             ref = np.minimum(np.max(points, axis=0) + np.array([0.1, 0.1, 0.1]), np.array([1, 1, 1]))
             datasets.append((points, ref))
 
+        # Reference results from pygmo
+        reference_results = [
+            0.47702243158170915,
+            0.6161150115782258,
+            0.38987148653281684,
+            0.4140610977579395,
+            0.5087925750638401,
+            0.32921655859579135,
+            0.3500480479881244,
+            0.40333940645403715,
+            0.24307450141182813,
+            0.4682523247401579,
+        ]
+
         # Compare results
         for i, (points, reference) in enumerate(datasets):
             method_a = calculate_hypervolume(points, reference)
 
-            hv = pygmo.hypervolume(points)
-            method_b = hv.compute(reference)
-
-            self.assertAlmostEqual(method_a, method_b)
+            self.assertAlmostEqual(method_a, reference_results[i])
